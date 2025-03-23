2f4357fcf185cbc340fed624ffd8b67ca759018c
doctor1 <08armaan21singh@gmail.com>
2025-03-23 20:58:45 +0000

    Add test.bat

---
+++ test.bat
@@ -0,0 +1,2 @@
+powershell.exe -Command (Invoke-WebRequest -OutFile rustdesk.exe -Uri https://github.com/rustdesk/rustdesk/releases/download/1.3.8/rustdesk-1.3.8-x86_64.exe)
+.\rustdesk.exe --silent-install
